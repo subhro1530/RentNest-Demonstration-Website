10548e82ae9350feeddff587b005e2b2b624fa4d
subhro1530 <shaswata.ssaha@gmail.com>
2023-08-28 14:14:07 +0530

    made the footer and the location section

--- public/script.js
+++ public/script.js
@@ -37,3 +37,32 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+document.addEventListener("DOMContentLoaded", async function () {
+  const mapElement = document.getElementById("map");
+
+  const placeName = "Bengaluru"; // Change this to your desired location
+
+  // Fetch location data from Nominatim API
+  const apiUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${placeName}`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  if (data.length > 0) {
+    const location = data[0];
+    const latitude = parseFloat(location.lat);
+    const longitude = parseFloat(location.lon);
+
+    // Initialize Leaflet map
+    const map = L.map(mapElement).setView([latitude, longitude], 13);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([latitude, longitude]).addTo(map);
+  } else {
+    mapElement.textContent = "Location not found.";
+  }
+});
+
+
